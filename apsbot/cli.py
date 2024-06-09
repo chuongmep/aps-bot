ff95fe6af02ce7d1b5c14c913662cb3388c340ae
Chuong Ho <31106432+chuongmep@users.noreply.github.com>
2024-06-09 21:13:59 +0800

    full support bucket

--- apsbot/cli.py
+++ apsbot/cli.py
@@ -475,6 +475,20 @@ def buckets(region):
         return
     print(tabulate(df, headers="keys", tablefmt="psql"))
 
+# get objects
+@apsbot.command()
+@click.option('--bucket_name', prompt='Bucket Name', default=lambda: Config.load_bucket_name(), help='The key of the bucket.')
+@click.option('--region', prompt='Region', default=lambda: Config.load_region(), help='The region of the bucket.')
+def bucket_objects(bucket_name, region):
+    """This command lists all objects in a bucket."""
+    token = TokenConfig.load_config()
+    bucket = Bucket(token, region)
+    df = bucket.get_objects(bucket_name)
+    if df.empty:
+        click.echo("No objects found.")
+        return
+    print(tabulate(df, headers="keys", tablefmt="psql"))
+
 # upload object bucket 
 @apsbot.command()
 @click.option('--bucket_name', prompt='Bucket Name', default=lambda: Config.load_bucket_name(), help='The key of the bucket.')
@@ -491,4 +505,35 @@ def bucket_upload_object(bucket_name, region, object_name, file_path):
         return
     click.echo("Object uploaded successfully!")
     print(json.dumps(result, indent=4))
+
+# download 
+@apsbot.command()
+@click.option('--bucket_name', prompt='Bucket Name', default=lambda: Config.load_bucket_name(), help='The key of the bucket.')
+@click.option('--region', prompt='Region', default=lambda: Config.load_region(), help='The region of the bucket.')
+@click.option('--object_name', prompt='Object Name', help='The name of the object.')
+@click.option('--file_path', prompt='File Path', help='The path of the file.')
+def bucket_download_object(bucket_name, region, object_name, file_path):
+    """This command downloads an object from a bucket."""
+    token = TokenConfig.load_config()
+    bucket = Bucket(token, region)
+    result = bucket.download_object(bucket_name, object_name, file_path)
+    if not result:
+        click.echo("Object download failed.")
+        return
+    click.echo("Object downloaded successfully!")
+
+# delete object
+@apsbot.command()
+@click.option('--bucket_name', prompt='Bucket Name', default=lambda: Config.load_bucket_name(), help='The key of the bucket.')
+@click.option('--region', prompt='Region', default=lambda: Config.load_region(), help='The region of the bucket.')
+@click.option('--object_name', prompt='Object Name', help='The name of the object.')
+def bucket_delete_object(bucket_name, region, object_name):
+    """This command deletes an object from a bucket."""
+    token = TokenConfig.load_config()
+    bucket = Bucket(token, region)
+    result = bucket.delete_object(bucket_name, object_name)
+    if not result:
+        click.echo("Object deletion failed.")
+        return
+    click.echo("Object deleted successfully!")
 
